6832fd72e6f0aefb11d4baacac645331a66f2087
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-09-13 09:59:46 +1000

    feat: Define Read route, create Marshmallow schema

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 app = Flask(__name__)
 #                                       database+driver://username:password@server:port/databasename
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://jun_user:123456@localhost:5432/jun_ecommerce"
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 # Create a model - Table representation
 class Product(db.Model):
@@ -18,6 +21,18 @@ class Product(db.Model):
     description = db.Column(db.String(255))
     price = db.Column(db.Float)
     stock = db.Column(db.Integer)
+
+class ProductSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+        load_instance = True
+
+# Create Marshmallow instances
+# To handle multiple products
+products_schema = ProductSchema(many=True)
+
+# To handle single product
+product_schema = ProductSchema()
 
 # Create commands to manipulate the DB, so it's easier to do some db stuffs
 
@@ -55,3 +70,22 @@ def seed_db():
     db.session.commit()
     # Acknowledgement message
     print("Table(s) seeded.")
+
+# CRUD Operations on Product
+# C - Create - POST
+# R - Read - GET
+# U - Update - PUT/PATCH
+# D - Delete - DELETE
+
+# Read from the Table
+# GET /products
+@app.route("/products")
+def get_products():
+    # stmt: SELECT * FROM products;
+    # products_list = db.select(Product)
+    products_list = Product.query.all()
+
+    # Convert the Python object to JSON serialisable format using Marshmallow
+    data = products_schema.dump(products_list)
+
+    return jsonify(data)
